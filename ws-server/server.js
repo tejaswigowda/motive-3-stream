fb2f44e090b7ed410bcb032f232ea7145fa0dc30
Tejaswi Gowda <tejaswi@asu.edu>
2022-11-07 09:52:28 -0700

    ac

--- ws-server/server.js
+++ ws-server/server.js
@@ -15,7 +15,7 @@ const wss2 = new WebSocket.Server({ noServer: true });
 //for motive 3
 wss1.on("connection", function connection(ws) {
   ws.on("message", function incoming(message) {
-    var x = message.trim().split("\t").join(" ");
+    var x = message.trim().split(" ").join(" ");
     //console.log(x);
     wss2.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
